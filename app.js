eeccef4874da2c4f46c2e5929ebde5d80f47b908
Triola Michelle Jackson <triolamichellejackson@Triolas-MacBook-Pro-2.local>
2021-12-02 15:21:34 -0600

    Added JS

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const greeting = document.getElementById("greeting");
 const christmasBtn = document.getElementById("christmas");
 const snowBtn = document.getElementById("snow");
 
+christmasBtn.addEventListener("click", () => {
+  body.classList.add("christmas");
+  body.classList.remove("snow");
+  greeting.innerHTML = "🎅🏽 Merry Christmas";
+});
+
+snowBtn.addEventListener("click", () => {
+  body.classList.add("snow");
+  body.classList.remove("christmas");
+  greeting.innerHTML = "❄️ Let it snow! ❄️";
+});
+
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
 
